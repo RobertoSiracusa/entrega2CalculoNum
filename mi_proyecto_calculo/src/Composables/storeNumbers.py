17996f340238641654ecf1243231e915c99cedbc
Felix Arocha <felixarocha1@gmail.com>
2025-07-25 21:58:27 -0400

    Archivo Modificado storeNumbers

--- mi_proyecto_calculo/src/Composables/storeNumbers.py
+++ mi_proyecto_calculo/src/Composables/storeNumbers.py
@@ -3,7 +3,7 @@ import random
 
 from Repositories.significantFigures import significantFigures
 from Repositories.numericSystem import numericSystem
-from Helpers.utils import logWriter, txtWriter
+from Helpers.utils import format2DArray, logWriter, txtWriter
 from Repositories.elementalOperation import elementalOperation
 from Repositories.gaussJordan import GaussJordan
 
@@ -24,8 +24,7 @@ def storeSignificantFigures(dataArray):
                 sf = significantFigures(value)
                 nS = numericSystem(value)
                 eO = elementalOperation(value, nS.binSystem, nS.decSystem, nS.hexSystem)
-                
-
+        
                 text= sf.toString()+"\n"+nS.toString()+"\n"+eO.toString()+"\n"+"Numero en Base 10:"+str(nS.getNumberBase10())+"\n"
                 txtWriter(outputFileName,text,True)
             except ValueError as e:
@@ -33,13 +32,16 @@ def storeSignificantFigures(dataArray):
                 continue
 
 def storeGaussJordan(dataArray):
-    currentDateTime = datetime.datetime.now()
-    formattedDateTime = currentDateTime.strftime("%Y-%m-%d_%H-%M-%S")
-    randNum = random.randint(1, 100)
-    #outputFileName = f"GaussJordan_{formattedDateTime}_serial{randNum}"
     outputFileName="GaussJordan"
     try:
         gJ = GaussJordan(dataArray)
         txtWriter(outputFileName, gJ.getSolution(), True)
+    except Exception as e:
+        logWriter(f"Error en Gauss-Jordan: {e}",True)
+
+def storeMatrix(dataArray,outputFileName):
+    try:
+        output=format2DArray(dataArray)
+        txtWriter(outputFileName, output, True)
     except Exception as e:
         logWriter(f"Error en Gauss-Jordan: {e}",True)
